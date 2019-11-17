f71158beb63d3ed16b68f657f50b4ae35d1fc6d1
Jeff Swisher <jefftswisher@gmail.com>
2019-11-17 16:55:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,14 +17,6 @@ def create_project_hash
   
   projects = {}
   
-  project.css("h2.bbcard_name strong a").text 
-  
-  project.css("div.project-thumbnail a img").attribute("src").value 
-  
-  project.css("p.bbcard_blurb").text 
-  
-  project.css("ul.project-meta span.location-name").text
-  
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
@@ -34,7 +26,5 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
-  
   projects 
-  
 end
